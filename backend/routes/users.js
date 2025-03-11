61fd164c2af7bd812cdb7ed0b157e43974728d3a
silvaturys <fernandes.arthur2704@gmail.com>
2025-03-10 22:43:12 -0300

    removing comments

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -49,25 +49,3 @@ router.patch(
 );
 
 module.exports = router;
-
-
-// const {Router} = require('express');
-// const { getUsers, getUserById, updateProfile, updateAvatar, getUserByMe } = require('../controllers/users');
-
-// const router = Router();
-
-// // Rota para obter todos os usuários
-// router.get("/", getUsers);
-
-// // Rota para obter um usuário por ID
-// router.get("/:id", getUserById);
-
-// // Rota para obter os dados do usuário atual (protegida)
-// console.log("teste2")
-// router.get("/me", getUserByMe); // Rota para o usuário autenticado
-
-// router.patch("/me", updateProfile); // Atualiza o perfil
-
-// router.patch("/me/avatar", updateAvatar); // Atualiza o avatar
-
-// module.exports = router;
